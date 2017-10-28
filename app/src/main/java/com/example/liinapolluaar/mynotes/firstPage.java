c06c0347430777d9a6da570fce31f93b532ce194
Liina Põlluäär <liinapolluaar@Liinas-MacBook-Air.local>
2017-10-28 12:12:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/liinapolluaar/mynotes/firstPage.java
@@ -0,0 +1,13 @@
+package com.example.liinapolluaar.mynotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class firstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
